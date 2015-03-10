b3f76a59da8784d8fd35c1d156c6b7f7ae2ade1a
Julien Viet <julien@julienviet.com>
2015-03-10 11:33:06 +0100

    Initial releaser

---
+++ clone.sh
@@ -0,0 +1,41 @@
+
+# Tools
+git clone --depth 1 git@github.com:vert-x3/vertx-codegen.git modules/vertx-codegen
+git clone --depth 1 git@github.com:vert-x3/vertx-docgen.git modules/vertx-docgen
+git clone --depth 1 git@github.com:vert-x3/vertx-codetrans.git modules/vertx-codetrans
+
+# Vert.x
+git clone --depth 1 git@github.com:eclipse/vert.x.git modules/vert.x
+
+# Lang
+git clone --depth 1 git@github.com:vert-x3/vertx-lang-js.git modules/vertx-lang-js
+git clone --depth 1 git@github.com:vert-x3/vertx-lang-groovy.git modules/vertx-lang-groovy
+
+# Base
+git clone --depth 1 git@github.com:vert-x3/vertx-unit.git modules/vertx-unit
+git clone --depth 1 git@github.com:vert-x3/vertx-metrics.git modules/vertx-metrics
+git clone --depth 1 git@github.com:vert-x3/vertx-rx.git modules/vertx-rx
+git clone --depth 1 git@github.com:vert-x3/vertx-reactive-streams.git modules/vertx-reactive-streams
+
+# Service
+git clone --depth 1 git@github.com:vert-x3/vertx-service-factory.git modules/vertx-service-factory
+git clone --depth 1 git@github.com:vert-x3/vertx-maven-service-factory.git modules/vertx-maven-service-factory
+git clone --depth 1 git@github.com:vert-x3/vertx-service-proxy.git modules/vertx-service-proxy
+
+# Clustering
+git clone --depth 1 git@github.com:vert-x3/vertx-hazelcast.git modules/vertx-hazelcast
+
+# Security
+git clone --depth 1 git@github.com:vert-x3/vertx-auth-service.git modules/vertx-auth-service
+
+# Db
+git clone --depth 1 git@github.com:vert-x3/vertx-sql-common.git modules/vertx-sql-common
+git clone --depth 1 git@github.com:vert-x3/vertx-jdbc-service.git modules/vertx-jdbc-service
+git clone --depth 1 git@github.com:vert-x3/vertx-embedded-mongo-db modules/vertx-embedded-mongo-db
+git clone --depth 1 git@github.com:vert-x3/vertx-mongo-service modules/vertx-mongo-service
+
+# Web
+git clone --depth 1 git@github.com:vert-x3/vertx-apex.git modules/vertx-apex
+
+# Stack
+# git clone --depth 1 git@github.com:vert-x3/vertx-stack.git modules/vertx-stack
---
+++ push.sh
@@ -0,0 +1,38 @@
+
+# Tools
+git --git-dir=modules/vertx-codegen/.git --work-tree=modules/vertx-codegen push origin master
+git --git-dir=modules/vertx-docgen/.git --work-tree=modules/vertx-docgen push origin master
+git --git-dir=modules/vertx-codetrans/.git --work-tree=modules/vertx-codetrans push origin master
+
+# Vert.x
+git --git-dir=modules/vert.x/.git --work-tree=modules/vert.x push origin master
+
+# Lang
+git --git-dir=modules/vertx-lang-js/.git --work-tree=modules/vertx-lang-js push origin master
+git --git-dir=modules/vertx-lang-groovy/.git --work-tree=modules/vertx-lang-groovy push origin master
+
+# Base
+git --git-dir=modules/vertx-unit/.git --work-tree=modules/vertx-unit push origin master
+git --git-dir=modules/vertx-metrics/.git --work-tree=modules/vertx-metrics push origin master
+git --git-dir=modules/vertx-rx/.git --work-tree=modules/vertx-rx push origin master
+git --git-dir=modules/vertx-reactive-streams/.git --work-tree=modules/vertx-reactive-streams push origin master
+
+# Service
+git --git-dir=modules/vertx-service-factory/.git --work-tree=modules/vertx-service-factory push origin master
+git --git-dir=modules/vertx-maven-service-factory/.git --work-tree=modules/vertx-maven-service-factory push origin master
+git --git-dir=modules/vertx-service-proxy/.git --work-tree=modules/vertx-service-proxy push origin master
+
+# Clustering
+git --git-dir=modules/vertx-hazelcast/.git --work-tree=modules/vertx-hazelcast push origin master
+
+# Security
+git --git-dir=modules/vertx-auth-service/.git --work-tree=modules/vertx-auth-service push origin master
+
+# Db
+git --git-dir=modules/vertx-sql-common/.git --work-tree=modules/vertx-sql-common push origin master
+git --git-dir=modules/vertx-jdbc-service/.git --work-tree=modules/vertx-jdbc-service push origin master
+git --git-dir=modules/vertx-embedded-mongo-db/.git --work-tree=modules/vertx-embedded-mongo-db push origin master
+git --git-dir=modules/vertx-mongo-service/.git --work-tree=modules/vertx-mongo-service push origin master
+
+# Web
+git --git-dir=modules/vertx-apex/.git --work-tree=modules/vertx-apex push origin master
