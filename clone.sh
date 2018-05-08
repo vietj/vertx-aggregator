03811a6ec1c94195ae645c372aecd586c16e5981
Julien Viet <julien@julienviet.com>
2018-05-08 12:02:37 +0200

    Adapt to latest 3.5.x structure

--- clone.sh
+++ clone.sh
@@ -1,81 +1,80 @@
+branch='3.5'
 
 # Tools
-git clone --depth 1 git@github.com:vert-x3/vertx-codegen.git modules/vertx-codegen
-git clone --depth 1 git@github.com:vert-x3/vertx-docgen.git modules/vertx-docgen
-git clone --depth 1 git@github.com:vert-x3/vertx-codetrans.git modules/vertx-codetrans
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-codegen.git modules/vertx-codegen
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-codetrans.git modules/vertx-codetrans
 
 # Vert.x
-git clone --depth 1 git@github.com:eclipse/vert.x.git modules/vert.x
+git clone -b $branch --depth 1 git@github.com:eclipse/vert.x.git modules/vert.x
 
 # Lang
-git clone --depth 1 git@github.com:vert-x3/vertx-lang-js.git modules/vertx-lang-js
-git clone --depth 1 git@github.com:vert-x3/vertx-lang-groovy.git modules/vertx-lang-groovy
-git clone --depth 1 git@github.com:vert-x3/vertx-lang-ruby.git modules/vertx-lang-ruby
-git clone --depth 1 git@github.com:vert-x3/vertx-lang-ceylon.git modules/vertx-lang-ceylon
-git clone --depth 1 git@github.com:vert-x3/vertx-lang-kotlin.git modules/vertx-lang-kotlin
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-lang-js.git modules/vertx-lang-js
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-lang-groovy.git modules/vertx-lang-groovy
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-lang-ruby.git modules/vertx-lang-ruby
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-lang-ceylon.git modules/vertx-lang-ceylon
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-lang-kotlin.git modules/vertx-lang-kotlin
 
 # Base
-git clone --depth 1 git@github.com:vert-x3/vertx-unit.git modules/vertx-unit
-git clone --depth 1 git@github.com:vert-x3/vertx-junit5.git modules/vertx-junit5
-git clone --depth 1 git@github.com:vert-x3/vertx-rx.git modules/vertx-rx
-git clone --depth 1 git@github.com:vert-x3/vertx-reactive-streams.git modules/vertx-reactive-streams
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-unit.git modules/vertx-unit
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-junit5.git modules/vertx-junit5
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-rx.git modules/vertx-rx
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-reactive-streams.git modules/vertx-reactive-streams
 
 # Devops
-git clone --depth 1 git@github.com:vert-x3/vertx-shell.git modules/vertx-shell
-git clone --depth 1 git@github.com:vert-x3/vertx-dropwizard-metrics.git modules/vertx-dropwizard-metrics
-git clone --depth 1 git@github.com:vert-x3/vertx-hawkular-metrics.git modules/vertx-hawkular-metrics
-git clone --depth 1 git@github.com:vert-x3/vertx-health-check.git modules/vertx-health-check
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-shell.git modules/vertx-shell
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-dropwizard-metrics.git modules/vertx-dropwizard-metrics
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-micrometer-metrics.git modules/vertx-micrometer-metrics
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-hawkular-metrics.git modules/vertx-hawkular-metrics
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-health-check.git modules/vertx-health-check
 
 # Programming
-git clone --depth 1 git@github.com:vert-x3/vertx-sync.git modules/vertx-sync
-git clone --depth 1 git@github.com:vert-x3/vertx-circuit-breaker.git modules/vertx-circuit-breaker
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-sync.git modules/vertx-sync
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-circuit-breaker.git modules/vertx-circuit-breaker
 
 # Service
-git clone --depth 1 git@github.com:vert-x3/vertx-service-factory.git modules/vertx-service-factory
-git clone --depth 1 git@github.com:vert-x3/vertx-maven-service-factory.git modules/vertx-maven-service-factory
-git clone --depth 1 git@github.com:vert-x3/vertx-http-service-factory.git modules/vertx-http-service-factory
-git clone --depth 1 git@github.com:vert-x3/vertx-service-proxy.git modules/vertx-service-proxy
-git clone --depth 1 git@github.com:vert-x3/vertx-service-discovery.git modules/vertx-service-discovery
-git clone --depth 1 git@github.com:vert-x3/vertx-grpc.git modules/vertx-grpc
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-service-factory.git modules/vertx-service-factory
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-maven-service-factory.git modules/vertx-maven-service-factory
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-http-service-factory.git modules/vertx-http-service-factory
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-service-proxy.git modules/vertx-service-proxy
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-service-discovery.git modules/vertx-service-discovery
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-grpc.git modules/vertx-grpc
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-client-services.git modules/vertx-client-services
 
 # IoT
-git clone --depth 1 git@github.com:vert-x3/vertx-mqtt.git modules/vertx-mqtt
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-mqtt.git modules/vertx-mqtt
 
 # Clustering
-git clone --depth 1 git@github.com:vert-x3/vertx-hazelcast.git modules/vertx-hazelcast
-git clone --depth 1 git@github.com:vert-x3/vertx-infinispan.git modules/vertx-infinispan
-git clone --depth 1 git@github.com:vert-x3/vertx-ignite.git modules/vertx-ignite
-git clone --depth 1 git@github.com:vert-x3/vertx-zookeeper.git modules/vertx-zookeeper
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-hazelcast.git modules/vertx-hazelcast
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-infinispan.git modules/vertx-infinispan
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-ignite.git modules/vertx-ignite
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-zookeeper.git modules/vertx-zookeeper
 
 # Security
-git clone --depth 1 git@github.com:vert-x3/vertx-auth.git modules/vertx-auth
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-auth.git modules/vertx-auth
 
-# Db
-git clone --depth 1 git@github.com:vert-x3/vertx-sql-common.git modules/vertx-sql-common
-git clone --depth 1 git@github.com:vert-x3/vertx-jdbc-client.git modules/vertx-jdbc-client
-git clone --depth 1 git@github.com:vert-x3/vertx-mysql-postgresql-client.git modules/vertx-mysql-postgresql-client
-git clone --depth 1 git@github.com:vert-x3/vertx-embedded-mongo-db.git modules/vertx-embedded-mongo-db
-git clone --depth 1 git@github.com:vert-x3/vertx-mongo-client.git modules/vertx-mongo-client
-git clone --depth 1 git@github.com:vert-x3/vertx-redis-client.git modules/vertx-redis-client
+# Clients
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-sql-common.git modules/vertx-sql-common
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-jdbc-client.git modules/vertx-jdbc-client
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-mysql-postgresql-client.git modules/vertx-mysql-postgresql-client
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-embedded-mongo-db.git modules/vertx-embedded-mongo-db
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-mongo-client.git modules/vertx-mongo-client
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-redis-client.git modules/vertx-redis-client
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-mail-client.git modules/vertx-mail-client
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-rabbitmq-client.git modules/vertx-rabbitmq-client
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-kafka-client.git modules/vertx-kafka-client
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-consul-client.git modules/vertx-consul-client
 
 # Messaging & Integration
-git clone --depth 1 git@github.com:vert-x3/vertx-bridge-common.git modules/vertx-bridge-common
-git clone --depth 1 git@github.com:vert-x3/vertx-tcp-eventbus-bridge.git modules/vertx-tcp-eventbus-bridge
-git clone --depth 1 git@github.com:vert-x3/vertx-stomp.git modules/vertx-stomp
-git clone --depth 1 git@github.com:vert-x3/vertx-jca.git modules/vertx-jca
-git clone --depth 1 git@github.com:vert-x3/vertx-mail-client.git modules/vertx-mail-client
-git clone --depth 1 git@github.com:vert-x3/vertx-rabbitmq-client.git modules/vertx-rabbitmq-client
-git clone --depth 1 git@github.com:vert-x3/vertx-proton.git modules/vertx-proton
-git clone --depth 1 git@github.com:vert-x3/vertx-amqp-bridge.git modules/vertx-amqp-bridge
-git clone --depth 1 git@github.com:vert-x3/vertx-camel-bridge.git modules/vertx-camel-bridge
-git clone --depth 1 git@github.com:vert-x3/vertx-kafka-client.git modules/vertx-kafka-client
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-bridge-common.git modules/vertx-bridge-common
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-tcp-eventbus-bridge.git modules/vertx-tcp-eventbus-bridge
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-stomp.git modules/vertx-stomp
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-jca.git modules/vertx-jca
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-proton.git modules/vertx-proton
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-amqp-bridge.git modules/vertx-amqp-bridge
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-camel-bridge.git modules/vertx-camel-bridge
 
 # Web
-git clone --depth 1 git@github.com:vert-x3/vertx-web.git modules/vertx-web
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-web.git modules/vertx-web
 
 # Other
-git clone --depth 1 git@github.com:vert-x3/vertx-consul-client.git modules/vertx-consul-client
-git clone --depth 1 git@github.com:vert-x3/vertx-config.git modules/vertx-config
-
-# Stack
-# git clone --depth 1 git@github.com:vert-x3/vertx-stack.git modules/vertx-stack
+git clone -b $branch --depth 1 git@github.com:vert-x3/vertx-config.git modules/vertx-config
--- push.sh
+++ push.sh
@@ -1,78 +1,80 @@
+branch='3.5'
 
 # Tools
-git --git-dir=modules/vertx-codegen/.git --work-tree=modules/vertx-codegen push --tags origin master
-git --git-dir=modules/vertx-docgen/.git --work-tree=modules/vertx-docgen push --tags origin master
-git --git-dir=modules/vertx-codetrans/.git --work-tree=modules/vertx-codetrans push --tags origin master
+git --git-dir=modules/vertx-codegen/.git --work-tree=modules/vertx-codegen push --tags origin $branch
+git --git-dir=modules/vertx-codetrans/.git --work-tree=modules/vertx-codetrans push --tags origin $branch
 
 # Vert.x
-git --git-dir=modules/vert.x/.git --work-tree=modules/vert.x push --tags origin master
+git --git-dir=modules/vert.x/.git --work-tree=modules/vert.x push --tags origin $branch
 
 # Lang
-git --git-dir=modules/vertx-lang-js/.git --work-tree=modules/vertx-lang-js push --tags origin master
-git --git-dir=modules/vertx-lang-groovy/.git --work-tree=modules/vertx-lang-groovy push --tags origin master
-git --git-dir=modules/vertx-lang-ruby/.git --work-tree=modules/vertx-lang-ruby push --tags origin master
-git --git-dir=modules/vertx-lang-ceylon/.git --work-tree=modules/vertx-lang-ceylon push --tags origin master
-git --git-dir=modules/vertx-lang-kotlin/.git --work-tree=modules/vertx-lang-kotlin push --tags origin master
+git --git-dir=modules/vertx-lang-js/.git --work-tree=modules/vertx-lang-js push --tags origin $branch
+git --git-dir=modules/vertx-lang-groovy/.git --work-tree=modules/vertx-lang-groovy push --tags origin $branch
+git --git-dir=modules/vertx-lang-ruby/.git --work-tree=modules/vertx-lang-ruby push --tags origin $branch
+git --git-dir=modules/vertx-lang-ceylon/.git --work-tree=modules/vertx-lang-ceylon push --tags origin $branch
+git --git-dir=modules/vertx-lang-kotlin/.git --work-tree=modules/vertx-lang-kotlin push --tags origin $branch
 
 # Base
-git --git-dir=modules/vertx-unit/.git --work-tree=modules/vertx-unit push --tags origin master
-git --git-dir=modules/vertx-junit5/.git --work-tree=modules/vertx-junit5 push --tags origin master
-git --git-dir=modules/vertx-rx/.git --work-tree=modules/vertx-rx push --tags origin master
-git --git-dir=modules/vertx-reactive-streams/.git --work-tree=modules/vertx-reactive-streams push --tags origin master
+git --git-dir=modules/vertx-unit/.git --work-tree=modules/vertx-unit push --tags origin $branch
+git --git-dir=modules/vertx-junit5/.git --work-tree=modules/vertx-junit5 push --tags origin $branch
+git --git-dir=modules/vertx-rx/.git --work-tree=modules/vertx-rx push --tags origin $branch
+git --git-dir=modules/vertx-reactive-streams/.git --work-tree=modules/vertx-reactive-streams push --tags origin $branch
 
 # Devops
-git --git-dir=modules/vertx-shell/.git --work-tree=modules/vertx-shell push --tags origin master
-git --git-dir=modules/vertx-dropwizard-metrics/.git --work-tree=modules/vertx-dropwizard-metrics push --tags origin master
-git --git-dir=modules/vertx-hawkular-metrics/.git --work-tree=modules/vertx-hawkular-metrics push --tags origin master
-git --git-dir=modules/vertx-health-check/.git --work-tree=modules/vertx-health-check push --tags origin master
+git --git-dir=modules/vertx-shell/.git --work-tree=modules/vertx-shell push --tags origin $branch
+git --git-dir=modules/vertx-dropwizard-metrics/.git --work-tree=modules/vertx-dropwizard-metrics push --tags origin $branch
+git --git-dir=modules/vertx-micrometer-metrics/.git --work-tree=modules/vertx-micrometer-metrics push --tags origin $branch
+git --git-dir=modules/vertx-hawkular-metrics/.git --work-tree=modules/vertx-hawkular-metrics push --tags origin $branch
+git --git-dir=modules/vertx-health-check/.git --work-tree=modules/vertx-health-check push --tags origin $branch
 
 # Programming
-git --git-dir=modules/vertx-sync/.git --work-tree=modules/vertx-sync push --tags origin master
-git --git-dir=modules/vertx-circuit-breaker/.git --work-tree=modules/vertx-circuit-breaker push --tags origin master
+git --git-dir=modules/vertx-sync/.git --work-tree=modules/vertx-sync push --tags origin $branch
+git --git-dir=modules/vertx-circuit-breaker/.git --work-tree=modules/vertx-circuit-breaker push --tags origin $branch
 
 # Service
-git --git-dir=modules/vertx-service-factory/.git --work-tree=modules/vertx-service-factory push --tags origin master
-git --git-dir=modules/vertx-maven-service-factory/.git --work-tree=modules/vertx-maven-service-factory push --tags origin master
-git --git-dir=modules/vertx-http-service-factory/.git --work-tree=modules/vertx-http-service-factory push --tags origin master
-git --git-dir=modules/vertx-service-proxy/.git --work-tree=modules/vertx-service-proxy push --tags origin master
-git --git-dir=modules/vertx-service-discovery/.git --work-tree=modules/vertx-service-discovery push --tags origin master
-git --git-dir=modules/vertx-grpc/.git --work-tree=modules/vertx-grpc push --tags origin master
+git --git-dir=modules/vertx-service-factory/.git --work-tree=modules/vertx-service-factory push --tags origin $branch
+git --git-dir=modules/vertx-maven-service-factory/.git --work-tree=modules/vertx-maven-service-factory push --tags origin $branch
+git --git-dir=modules/vertx-http-service-factory/.git --work-tree=modules/vertx-http-service-factory push --tags origin $branch
+git --git-dir=modules/vertx-service-proxy/.git --work-tree=modules/vertx-service-proxy push --tags origin $branch
+git --git-dir=modules/vertx-service-discovery/.git --work-tree=modules/vertx-service-discovery push --tags origin $branch
+git --git-dir=modules/vertx-grpc/.git --work-tree=modules/vertx-grpc push --tags origin $branch
+git --git-dir=modules/vertx-client-services/.git --work-tree=modules/vertx-client-services push --tags origin $branch
 
 # IoT
-git --git-dir=modules/vertx-mqtt/.git --work-tree=modules/vertx-mqtt push --tags origin master
+git --git-dir=modules/vertx-mqtt/.git --work-tree=modules/vertx-mqtt push --tags origin $branch
 
 # Clustering
-git --git-dir=modules/vertx-hazelcast/.git --work-tree=modules/vertx-hazelcast push --tags origin master
-git --git-dir=modules/vertx-infinispan/.git --work-tree=modules/vertx-infinispan push --tags origin master
-git --git-dir=modules/vertx-ignite/.git --work-tree=modules/vertx-ignite push --tags origin master
-git --git-dir=modules/vertx-zookeeper/.git --work-tree=modules/vertx-zookeeper push --tags origin master
+git --git-dir=modules/vertx-hazelcast/.git --work-tree=modules/vertx-hazelcast push --tags origin $branch
+git --git-dir=modules/vertx-infinispan/.git --work-tree=modules/vertx-infinispan push --tags origin $branch
+git --git-dir=modules/vertx-ignite/.git --work-tree=modules/vertx-ignite push --tags origin $branch
+git --git-dir=modules/vertx-zookeeper/.git --work-tree=modules/vertx-zookeeper push --tags origin $branch
 
 # Security
-git --git-dir=modules/vertx-auth/.git --work-tree=modules/vertx-auth push --tags origin master
+git --git-dir=modules/vertx-auth/.git --work-tree=modules/vertx-auth push --tags origin $branch
 
-# Db
-git --git-dir=modules/vertx-sql-common/.git --work-tree=modules/vertx-sql-common push --tags origin master
-git --git-dir=modules/vertx-jdbc-client/.git --work-tree=modules/vertx-jdbc-client push --tags origin master
-git --git-dir=modules/vertx-mysql-postgresql-client/.git --work-tree=modules/vertx-mysql-postgresql-client push --tags origin master
-git --git-dir=modules/vertx-embedded-mongo-db/.git --work-tree=modules/vertx-embedded-mongo-db push --tags origin master
-git --git-dir=modules/vertx-mongo-client/.git --work-tree=modules/vertx-mongo-client push --tags origin master
-git --git-dir=modules/vertx-redis-client/.git --work-tree=modules/vertx-redis-client push --tags origin master
+# Clients
+git --git-dir=modules/vertx-sql-common/.git --work-tree=modules/vertx-sql-common push --tags origin $branch
+git --git-dir=modules/vertx-jdbc-client/.git --work-tree=modules/vertx-jdbc-client push --tags origin $branch
+git --git-dir=modules/vertx-mysql-postgresql-client/.git --work-tree=modules/vertx-mysql-postgresql-client push --tags origin $branch
+git --git-dir=modules/vertx-embedded-mongo-db/.git --work-tree=modules/vertx-embedded-mongo-db push --tags origin $branch
+git --git-dir=modules/vertx-mongo-client/.git --work-tree=modules/vertx-mongo-client push --tags origin $branch
+git --git-dir=modules/vertx-redis-client/.git --work-tree=modules/vertx-redis-client push --tags origin $branch
+git --git-dir=modules/vertx-mail-client/.git --work-tree=modules/vertx-mail-client push --tags origin $branch
+git --git-dir=modules/vertx-rabbitmq-client/.git --work-tree=modules/vertx-rabbitmq-client push --tags origin $branch
+git --git-dir=modules/vertx-kafka-client/.git --work-tree=modules/vertx-kafka-client push --tags origin $branch
+git --git-dir=modules/vertx-consul-client/.git --work-tree=modules/vertx-consul-client push --tags origin $branch
 
 # Messaging & Integration
-git --git-dir=modules/vertx-bridge-common/.git --work-tree=modules/vertx-bridge-common push --tags origin master
-git --git-dir=modules/vertx-tcp-eventbus-bridge/.git --work-tree=modules/vertx-tcp-eventbus-bridge push --tags origin master
-git --git-dir=modules/vertx-stomp/.git --work-tree=modules/vertx-stomp push --tags origin master
-git --git-dir=modules/vertx-jca/.git --work-tree=modules/vertx-jca push --tags origin master
-git --git-dir=modules/vertx-mail-client/.git --work-tree=modules/vertx-mail-client push --tags origin master
-git --git-dir=modules/vertx-rabbitmq-client/.git --work-tree=modules/vertx-rabbitmq-client push --tags origin master
-git --git-dir=modules/vertx-proton/.git --work-tree=modules/vertx-proton push --tags origin master
-git --git-dir=modules/vertx-amqp-bridge/.git --work-tree=modules/vertx-amqp-bridge push --tags origin master
-git --git-dir=modules/vertx-camel-bridge/.git --work-tree=modules/vertx-camel-bridge push --tags origin master
-git --git-dir=modules/vertx-kafka-client/.git --work-tree=modules/vertx-kafka-client push --tags origin master
+git --git-dir=modules/vertx-bridge-common/.git --work-tree=modules/vertx-bridge-common push --tags origin $branch
+git --git-dir=modules/vertx-tcp-eventbus-bridge/.git --work-tree=modules/vertx-tcp-eventbus-bridge push --tags origin $branch
+git --git-dir=modules/vertx-stomp/.git --work-tree=modules/vertx-stomp push --tags origin $branch
+git --git-dir=modules/vertx-jca/.git --work-tree=modules/vertx-jca push --tags origin $branch
+git --git-dir=modules/vertx-proton/.git --work-tree=modules/vertx-proton push --tags origin $branch
+git --git-dir=modules/vertx-amqp-bridge/.git --work-tree=modules/vertx-amqp-bridge push --tags origin $branch
+git --git-dir=modules/vertx-camel-bridge/.git --work-tree=modules/vertx-camel-bridge push --tags origin $branch
 
 # Web
-git --git-dir=modules/vertx-web/.git --work-tree=modules/vertx-web push --tags origin master
+git --git-dir=modules/vertx-web/.git --work-tree=modules/vertx-web push --tags origin $branch
 
 # Other
-git --git-dir=modules/vertx-consul-client/.git --work-tree=modules/vertx-consul-client push --tags origin master
-git --git-dir=modules/vertx-config/.git --work-tree=modules/vertx-config push --tags origin master
+git --git-dir=modules/vertx-config/.git --work-tree=modules/vertx-config push --tags origin $branch
