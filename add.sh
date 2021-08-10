378231b296fbef35fdcc4747d070c26bd076c7cc
Julien Viet <julien@julienviet.com>
2021-08-10 16:29:44 +0200

    files to use new ci/cd files

---
+++ add.sh
@@ -0,0 +1,81 @@
+branch='master'
+branch2='main'
+
+# Tools
+git --git-dir=modules/vertx-codegen/.git --work-tree=modules/vertx-codegen add .
+
+# Vert.x
+git --git-dir=modules/vert.x/.git --work-tree=modules/vert.x add .
+
+# Lang
+git --git-dir=modules/vertx-lang-groovy/.git --work-tree=modules/vertx-lang-groovy add .
+git --git-dir=modules/vertx-lang-kotlin/.git --work-tree=modules/vertx-lang-kotlin add .
+
+# Base
+git --git-dir=modules/vertx-unit/.git --work-tree=modules/vertx-unit add .
+git --git-dir=modules/vertx-junit5/.git --work-tree=modules/vertx-junit5 add .
+git --git-dir=modules/vertx-rx/.git --work-tree=modules/vertx-rx add .
+git --git-dir=modules/vertx-reactive-streams/.git --work-tree=modules/vertx-reactive-streams add .
+
+# Observability
+git --git-dir=modules/vertx-dropwizard-metrics/.git --work-tree=modules/vertx-dropwizard-metrics add .
+git --git-dir=modules/vertx-micrometer-metrics/.git --work-tree=modules/vertx-micrometer-metrics add .
+git --git-dir=modules/vertx-health-check/.git --work-tree=modules/vertx-health-check add .
+git --git-dir=modules/vertx-tracing/.git --work-tree=modules/vertx-tracing add .
+
+# Devops
+git --git-dir=modules/vertx-shell/.git --work-tree=modules/vertx-shell add .
+
+# Programming
+git --git-dir=modules/vertx-sync/.git --work-tree=modules/vertx-sync add .
+git --git-dir=modules/vertx-circuit-breaker/.git --work-tree=modules/vertx-circuit-breaker add .
+
+# Service
+git --git-dir=modules/vertx-service-factory/.git --work-tree=modules/vertx-service-factory add .
+git --git-dir=modules/vertx-maven-service-factory/.git --work-tree=modules/vertx-maven-service-factory add .
+git --git-dir=modules/vertx-http-service-factory/.git --work-tree=modules/vertx-http-service-factory add .
+git --git-dir=modules/vertx-service-proxy/.git --work-tree=modules/vertx-service-proxy add .
+git --git-dir=modules/vertx-sockjs-service-proxy/.git --work-tree=modules/vertx-sockjs-service-proxy add .
+git --git-dir=modules/vertx-service-discovery/.git --work-tree=modules/vertx-service-discovery add .
+git --git-dir=modules/vertx-grpc/.git --work-tree=modules/vertx-grpc add .
+
+# IoT
+git --git-dir=modules/vertx-mqtt/.git --work-tree=modules/vertx-mqtt add .
+
+# Clustering
+git --git-dir=modules/vertx-hazelcast/.git --work-tree=modules/vertx-hazelcast add .
+git --git-dir=modules/vertx-infinispan/.git --work-tree=modules/vertx-infinispan add .
+git --git-dir=modules/vertx-ignite/.git --work-tree=modules/vertx-ignite add .
+git --git-dir=modules/vertx-zookeeper/.git --work-tree=modules/vertx-zookeeper add .
+
+# Security
+git --git-dir=modules/vertx-auth/.git --work-tree=modules/vertx-auth add .
+
+# Clients
+git --git-dir=modules/vertx-jdbc-client/.git --work-tree=modules/vertx-jdbc-client add .
+git --git-dir=modules/vertx-sql-client/.git --work-tree=modules/vertx-sql-client add .
+git --git-dir=modules/vertx-mongo-client/.git --work-tree=modules/vertx-mongo-client add .
+git --git-dir=modules/vertx-redis-client/.git --work-tree=modules/vertx-redis-client add .
+git --git-dir=modules/vertx-mail-client/.git --work-tree=modules/vertx-mail-client add .
+git --git-dir=modules/vertx-rabbitmq-client/.git --work-tree=modules/vertx-rabbitmq-client add .
+git --git-dir=modules/vertx-kafka-client/.git --work-tree=modules/vertx-kafka-client add .
+git --git-dir=modules/vertx-consul-client/.git --work-tree=modules/vertx-consul-client add .
+git --git-dir=modules/vertx-cassandra-client/.git --work-tree=modules/vertx-cassandra-client add .
+
+# Messaging & Integration
+git --git-dir=modules/vertx-bridge-common/.git --work-tree=modules/vertx-bridge-common add .
+git --git-dir=modules/vertx-tcp-eventbus-bridge/.git --work-tree=modules/vertx-tcp-eventbus-bridge add .
+git --git-dir=modules/vertx-stomp/.git --work-tree=modules/vertx-stomp add .
+git --git-dir=modules/vertx-proton/.git --work-tree=modules/vertx-proton add .
+git --git-dir=modules/vertx-amqp-client/.git --work-tree=modules/vertx-amqp-client add .
+git --git-dir=modules/vertx-camel-bridge/.git --work-tree=modules/vertx-camel-bridge add .
+
+# Json Schema
+git --git-dir=modules/vertx-json-schema/.git --work-tree=modules/vertx-json-schema add .
+
+# Web
+git --git-dir=modules/vertx-http-proxy/.git --work-tree=modules/vertx-http-proxy add .
+git --git-dir=modules/vertx-web/.git --work-tree=modules/vertx-web add .
+
+# Other
+git --git-dir=modules/vertx-config/.git --work-tree=modules/vertx-config add .
---
+++ copy2.sh
@@ -0,0 +1,81 @@
+branch='master'
+branch2='main'
+
+# Tools
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-codegen/.github/
+
+# Vert.x
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vert.x/.github/
+
+# Lang
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-lang-groovy/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-lang-kotlin/.github/
+
+# Base
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-unit/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-junit5/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-rx/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-reactive-streams/.github/
+
+# Observability
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-dropwizard-metrics/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-micrometer-metrics/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-health-check/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-tracing/.github/
+
+# Devops
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-shell/.github/
+
+# Programming
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-sync/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-circuit-breaker/.github/
+
+# Service
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-service-factory/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-maven-service-factory/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-http-service-factory/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-service-proxy/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-sockjs-service-proxy/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-service-discovery/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-grpc/.github/
+
+# IoT
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-mqtt/.github/
+
+# Clustering
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-hazelcast/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-infinispan/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-ignite/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-zookeeper/.github/
+
+# Security
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-auth/.github/
+
+# Clients
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-jdbc-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-sql-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-mongo-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-redis-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-mail-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-rabbitmq-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-kafka-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-consul-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-cassandra-client/.github/
+
+# Messaging & Integration
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-bridge-common/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-tcp-eventbus-bridge/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-stomp/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-proton/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-amqp-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-camel-bridge/.github/
+
+# Json Schema
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-json-schema/.github/
+
+# Web
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-http-proxy/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-web/.github/
+
+# Other
+cp /Users/julien/java/vertx-junit5/.github/maven-cd-settings.xml modules/vertx-config/.github/
