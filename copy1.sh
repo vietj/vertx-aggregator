7f1ef478d5f48a47e4ebf59df46d5a52e7bd1230
Julien Viet <julien@julienviet.com>
2021-08-11 09:46:24 +0200

    start to use new ci

---
+++ copy1.sh
@@ -0,0 +1,82 @@
+branch='3.9'
+
+# Tools
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-codegen/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-codetrans/.github/
+
+# Vert.x
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vert.x/.github/
+
+# Lang
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-lang-js/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-lang-groovy/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-lang-ruby/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-lang-kotlin/.github/
+
+# Base
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-unit/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-junit5/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-rx/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-reactive-streams/.github/
+
+# Devops
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-shell/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-dropwizard-metrics/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-micrometer-metrics/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-health-check/.github/
+
+# Programming
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-sync/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-circuit-breaker/.github/
+
+# Service
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-service-factory/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-maven-service-factory/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-http-service-factory/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-service-proxy/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-sockjs-service-proxy/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-service-discovery/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-grpc/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-client-services/.github/
+
+# IoT
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-mqtt/.github/
+
+# Clustering
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-hazelcast/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-infinispan/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-ignite/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-zookeeper/.github/
+
+# Security
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-auth/.github/
+
+# Clients
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-sql-common/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xmlmodules/vertx-jdbc-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-mysql-postgresql-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-sql-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-embedded-mongo-db/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-mongo-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-redis-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-mail-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-rabbitmq-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-kafka-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-consul-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-cassandra-client/.github/
+
+# Messaging & Integration
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-bridge-common/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-tcp-eventbus-bridge/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-stomp/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-jca/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-proton/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-amqp-bridge/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-amqp-client/.github/
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-camel-bridge/.github/
+
+# Web
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-web/.github/
+
+# Other
+cp /Users/julien/java/vertx-junit5/.github/maven-ci-settings.xml modules/vertx-config/.github/
